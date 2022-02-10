1e86807b8465b30599e684c3b050f510f33b84e2
CJM1994 <connorjohnmullin@gmail.com>
2022-02-10 05:29:51 +0000

    function to calculate sales/tax data for one company

---
+++ sales.js
@@ -0,0 +1,54 @@
+const salesTaxRates = {
+  AB: 0.05,
+  BC: 0.12,
+  SK: 0.10
+};
+
+const companySalesData = [
+  {
+    name: "Telus",
+    province: "BC",
+    sales: [100, 200, 400]
+  },
+  {
+    name: "Bombardier",
+    province: "AB",
+    sales: [80, 20, 10, 100, 90, 500]
+  },
+  {
+    name: "Telus",
+    province: "SK",
+    sales: [500, 100]
+  }
+];
+
+const calculateCompanySalesData = function (taxRate, salesData) {
+
+  let totalTaxes = 0;
+  let totalSales = 0;
+
+  for (const sales of salesData) {
+    totalTaxes += (sales * taxRate);
+    totalSales += sales;
+  }
+
+  return { 'totalTaxes': totalTaxes, 'totalSales': totalSales };
+
+}
+
+const assignCalculatedSalesData = function (salesTaxRates, companySalesData) {
+
+  const processedSalesData = {};
+
+  for (const company in companySalesData) {
+    let companyName = companySalesData[company]['name'];
+    let taxRate = salesTaxRates[companySalesData[company]['province']];
+
+    processedSalesData[companyName] = calculateCompanySalesData(taxRate, [100, 200, 400]);
+  }
+
+  return processedSalesData;
+
+}
+
+console.log(assignCalculatedSalesData(salesTaxRates, companySalesData));
